de4cb6acf376c8d2275884f82e4dead020c86484
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-13 09:25:31 +0530

    feat: basic banner styles applied for the text

--- components/home/contact-section/index.tsx
+++ components/home/contact-section/index.tsx
@@ -4,6 +4,8 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useEffect } from 'react';
 
+import BannerText from '@/components/common/banner-text';
+
 import styles from './contact-section.module.scss';
 
 export interface IContactSectionProps {}
@@ -11,7 +13,7 @@ export interface IContactSectionProps {}
 export default function ContactSection(_props: IContactSectionProps) {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-
+    // FIXME: Aimation for the col1 text
     const tlHeading = gsap.timeline({
       scrollTrigger: {
         trigger: '#contact-section__text-wrapper',
@@ -27,7 +29,7 @@ export default function ContactSection(_props: IContactSectionProps) {
       // '#contact-section__text-title1',
       '.contact-section-text',
       { opacity: 0, y: 20 }, // from state
-      { opacity: 1, y: 0, duration: 0.5, delay: 0.5, stagger: 0.2 }, // to state
+      { opacity: 1, y: 0, duration: 0.5, delay: 0.5, stagger: 0.3 }, // to state
     );
   }, []);
 
@@ -36,51 +38,13 @@ export default function ContactSection(_props: IContactSectionProps) {
       <Grid columns={{ initial: '1', md: '1fr auto' }} gap={'9'}>
         {/* Col 1 */}
         <Box id="contact-section__text-wrapper" className={cx(styles['d-section__text-wrapper'])} grow={'1'}>
-          <Box className="flex justify-center ">
-            <Text
-              id="contact-section__text-title1"
-              as="span"
-              size={'9'}
-              weight={'bold'}
-              className={cx(styles['d-section__text-title1'], 'contact-section-text')}
-            >
-              Embark on a
-            </Text>
-          </Box>
-          <Box className="flex justify-center">
-            <Text
-              id="contact-section__text-title2"
-              as="span"
-              size={'9'}
-              weight={'bold'}
-              className={cx(styles['d-section__text-title2'], 'contact-section-text')}
-            >
-              Joruney of
-            </Text>
-          </Box>
-          <Box className="flex justify-center ">
-            <Text
-              id="contact-section__text-title3"
-              as="span"
-              size={'9'}
-              weight={'bold'}
-              className={cx(styles['d-section__text-title3'], 'contact-section-text')}
-            >
-              Unprecedented
-            </Text>
-          </Box>
-          <Box className="flex justify-center ">
-            <Text
-              id="contact-section__text-title3"
-              as="span"
-              size={'9'}
-              weight={'bold'}
-              className={cx(styles['d-section__text-title3'], 'contact-section-text')}
-            >
-              Growth
-            </Text>
+          <Box mb={'8'} className="text-center">
+            <BannerText wrapperClassName="about-section__heading" text="Embark on a journey of" textClassName="text-4xl" />
+            <BannerText wrapperClassName="about-section__heading" text="Unprecedented" textClassName="text-5xl" />
+            <BannerText wrapperClassName="about-section__heading" text="growth" textClassName="text-6xl" />
           </Box>
         </Box>
+
         {/* Col 2 */}
         <Card className={cx(styles['d-section__card'])}>
           <Box className={cx(styles['d-section__card__content'])}>
