007968eac89b095f14744c04062bf536974b36f0
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-06-01 14:21:12 +0530

    fix: mobile and tablet layout for benefits section

--- components/home/benefits-section/index.tsx
+++ components/home/benefits-section/index.tsx
@@ -16,81 +16,79 @@ export default function BenefitsSection(_props: IBenefitsSectionProps) {
         <Heading as="h3" mb={'6'} size={'8'} weight={'light'} className={cx(styles['d-section__heading'])}>
           We Can Improve Your Business Performance And Gain More Customers
         </Heading>
-        <Grid columns={{ initial: '1', md: '2' }} gap={'7'}>
+        <Grid columns={{ initial: '1', sm: '2' }} gap={'7'}>
           {/* Card 1 */}
           <Card variant="surface" className={cx(styles['d-section__card'])}>
-            <Flex gap={'6'} p={'4'}>
+            <Flex gap={'6'} mb={'4'} align={'center'}>
               {/* Col 1 */}
               <Box className={cx(styles['d-section__icon-wrapper'])}>
                 <FaUsers className={cx(styles['d-section__icon'])} />
               </Box>
               {/* Col 2 */}
-              <Box mr={'6'}>
-                <Text as="div" size={'5'} weight={'bold'} mb={'3'}>
-                  Experienced Team
-                </Text>
-                <Text as="div" size={'4'}>
-                  We have an experienced team of professionals and the total experience of the team is 72 years.
-                </Text>
-              </Box>
+              <Heading as="h4" size={'5'} weight={'bold'}>
+                Experienced Team
+              </Heading>
             </Flex>
+            <Text as="div" size={'4'}>
+              We have an experienced team of professionals and the total experience of the team is 72 years.
+            </Text>
           </Card>
 
           {/* Card 2 */}
           <Card variant="surface" className={cx(styles['d-section__card'])}>
-            <Flex gap={'6'} p={'4'}>
+            <Flex gap={'6'} mb={'4'} align={'center'}>
               {/* Col 1 */}
               <Box className={cx(styles['d-section__icon-wrapper'])}>
                 <BsFillBadge3dFill className={cx(styles['d-section__icon'])} />
               </Box>
               {/* Col 2 */}
               <Box mr={'6'}>
-                <Text as="div" size={'5'} weight={'bold'} mb={'3'}>
+                <Heading as="h4" size={'5'} weight={'bold'} mb={'3'}>
                   3D Services
-                </Text>
-                <Text as="div" size={'4'}>
-                  We provide a 3D website and 3D Animation along with all the other services needed for a business to grow online.
-                </Text>
+                </Heading>
               </Box>
             </Flex>
+            <Text as="div" size={'4'}>
+              We provide a 3D website and 3D Animation along with all the other services needed for a business to grow online.
+            </Text>
           </Card>
 
           {/* Card 3 */}
           <Card variant="surface" className={cx(styles['d-section__card'])}>
-            <Flex gap={'6'} p={'4'}>
+            <Flex gap={'6'} mb={'4'} align={'center'}>
               {/* Col 1 */}
               <Box className={cx(styles['d-section__icon-wrapper'])}>
                 <MdLooksOne className={cx(styles['d-section__icon'])} />
               </Box>
               {/* Col 2 */}
               <Box mr={'6'}>
-                <Text as="div" size={'5'} weight={'bold'} mb={'3'}>
+                <Heading as="h4" size={'5'} weight={'bold'} mb={'3'}>
                   One-Stop Solution
-                </Text>
-                <Text as="div" size={'4'}>
-                  We are your one-stop solution for websites, social media, and anything online.
-                </Text>
+                </Heading>
               </Box>
             </Flex>
+            <Text as="div" size={'4'}>
+              We are your one-stop solution for websites, social media, and anything online.
+            </Text>
           </Card>
 
           {/* Card 4 */}
           <Card variant="surface" className={cx(styles['d-section__card'])}>
-            <Flex gap={'6'} p={'4'}>
+            <Flex gap={'6'} mb={'4'} align={'center'}>
               {/* Col 1 */}
               <Box className={cx(styles['d-section__icon-wrapper'])}>
                 <FaGlobeAsia className={cx(styles['d-section__icon'])} />
               </Box>
               {/* Col 2 */}
               <Box mr={'6'}>
-                <Text as="div" size={'5'} weight={'bold'} mb={'3'}>
+                <Heading as="h4" size={'5'} weight={'bold'} mb={'3'}>
                   Global Reach
-                </Text>
-                <Text as="div" size={'4'}>
-                  Our services have been availed by prominent organizations in India and across the globe.
-                </Text>
+                </Heading>
               </Box>
             </Flex>
+            <Text as="div" size={'4'}>
+              Our services have been availed by prominent organizations in India and across the globe.
+            </Text>
           </Card>
         </Grid>
       </Box>
