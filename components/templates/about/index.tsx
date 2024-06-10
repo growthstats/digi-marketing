7e61178c10271af793326459641d1baaccba48e5
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-06-10 20:27:22 +0530

    feat: add contact section on about page

--- components/templates/about/index.tsx
+++ components/templates/about/index.tsx
@@ -3,6 +3,7 @@
 import { FC } from 'react';
 
 import AboutSection from '@/components/about/about-section';
+import ContactSection from '@/components/about/contact-section';
 import HeroSection from '@/components/about/hero-section';
 import TeamSection from '@/components/about/team-section';
 import VisionSection from '@/components/about/vision-section';
@@ -15,6 +16,7 @@ const AboutPageTemplate: FC<IAboutPageTemplateProps> = (_props) => (
     <AboutSection />
     <VisionSection />
     <TeamSection />
+    <ContactSection />
   </div>
 );
 
