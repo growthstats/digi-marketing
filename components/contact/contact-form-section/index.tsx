9d0a77f39b1431bed06825ff5183360b598a8dfd
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-07-03 20:59:00 +0530

    feat: finish contact info section

--- components/contact/contact-form-section/index.tsx
+++ components/contact/contact-form-section/index.tsx
@@ -1,11 +1,101 @@
-import { Section } from '@radix-ui/themes';
+import { Box, Button, Heading, Section, Text } from '@radix-ui/themes';
 import cx from 'classnames';
+import Link from 'next/link';
 import { FC } from 'react';
 
 import styles from './contact-form-section.module.scss';
 
 interface IContactFormSectionProps {}
 
-const ContactFormSection: FC<IContactFormSectionProps> = () => <Section className={cx(styles['d-section'])}>ContactFormSection</Section>;
+const ContactFormSection: FC<IContactFormSectionProps> = () => (
+  <Section className={cx(styles['d-section'])}>
+    {/* Form */}
+    <Box className={cx(styles['d-section__form-container'])}>
+      <Heading as="h3" className={cx(styles['d-section__form-heading'])}>
+        Message Us
+      </Heading>
+      <form className={cx(styles['d-section__contact-form'])}>
+        <Box className={cx(styles['d-section__form-fields-row'])}>
+          <Box className={cx(styles['d-section__form-fields-col'])}>
+            {/* Name */}
+            <Box className={cx(styles['d-section__form-control'])}>
+              <input name="cname" type="text" placeholder="Name" className={cx(styles['d-section__form-control-text-input'])} />
+              <label htmlFor="cname" className={cx(styles['d-section__form-control-label'])}>
+                Name
+              </label>
+            </Box>
+            {/* Email */}
+            <Box className={cx(styles['d-section__form-control'])}>
+              <input name="email" type="text" placeholder="Email" className={cx(styles['d-section__form-control-text-input'])} />
+              <label htmlFor="email" className={cx(styles['d-section__form-control-label'])}>
+                Email
+              </label>
+            </Box>
+            {/* Phone */}
+            <Box className={cx(styles['d-section__form-control'])}>
+              <input name="phone" type="text" placeholder="Phone" className={cx(styles['d-section__form-control-text-input'])} />
+              <label htmlFor="phone" className={cx(styles['d-section__form-control-label'])}>
+                Phone
+              </label>
+            </Box>
+          </Box>
+          <Box className={cx(styles['d-section__form-fields-col'])}>
+            {/* Message */}
+            <Box className={cx(styles['d-section__form-control'], 'h-full')}>
+              <textarea
+                name="message"
+                placeholder="Message"
+                className={cx(styles['d-section__form-control-text-input'], styles['d-section__form-control-textarea-input'])}
+              />
+              <label htmlFor="message" className={cx(styles['d-section__form-control-label'])}>
+                Message
+              </label>
+            </Box>
+          </Box>
+        </Box>
+        {/* Submit Form */}
+        <Box className={cx(styles['d-section__form-submit-wrapper'])}>
+          <Button size={'4'} className={cx(styles['d-section__form-submit'])} type="submit">
+            Submit
+          </Button>
+        </Box>
+      </form>
+    </Box>
+    {/* Contact Info */}
+    <Box className={cx(styles['d-section__info-container'])}>
+      <Box className={cx(styles['d-section__info-row'])}>
+        <Box className={cx(styles['d-section__info-col'])}>
+          <Heading as="h4" className={cx(styles['d-section__info-title'])}>
+            General Inquiries
+          </Heading>
+          <Box className={cx(styles['d-section__info-link'])}>
+            <Link href={'mailto:info@growthstats.com'}>
+              <Text as="p" size={'5'}>
+                info@growthstats.com
+              </Text>
+            </Link>
+          </Box>
+          <Box className={cx(styles['d-section__info-link'])}>
+            <Link href={'tel:876-876-7654'}>
+              <Text as="p" size={'5'}>
+                Tel: 876-876-7654
+              </Text>
+            </Link>
+          </Box>
+        </Box>
+        <Box className={cx(styles['d-section__info-col'])}>
+          <Heading as="h4" className={cx(styles['d-section__info-title'])}>
+            Location
+          </Heading>
+          <Box className={cx(styles['d-section__info-address'])}>
+            <Text as="p" size={'5'}>
+              B.T Kawade Road, Sai Sulochana, opposite to shirke company, Ganesh Nagar, Ghorpadi, Pune, Maharashtra 411036
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  </Section>
+);
 
 export default ContactFormSection;
