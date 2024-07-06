59366c8ad167266deef6a01bfd942046f02dc4e2
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-07-06 19:09:22 +0530

    feat: add og and twitter

---
+++ app/about/twitter-image.tsx
@@ -0,0 +1,13 @@
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const alt = 'About Growth Stats';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+export default function Image() {}
