e560b39272caaecdef36b71f5563fc382deb15c7
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-07-29 16:23:48 +0530

    fix: template page transition wrapper animation

--- app/(site)/template.tsx
+++ app/(site)/template.tsx
@@ -6,10 +6,7 @@ import { useRef } from 'react';
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const containerRef = useRef(null);
-  const banner1Ref = useRef(null);
-  const banner2Ref = useRef(null);
-  const banner3Ref = useRef(null);
-  const banner4Ref = useRef(null);
+  const wrapperRef = useRef(null);
 
   useGSAP(
     () => {
@@ -21,19 +18,30 @@ export default function Template({ children }: { children: React.ReactNode }) {
         yPercent: 100,
         stagger: 0.2,
       });
+
+      // Ensure to include the animation of the wrapper to disappear after the banners have animated
+      tl.to(wrapperRef.current, {
+        opacity: 0,
+        duration: 0.5,
+        onComplete: () => {
+          // This will ensure the wrapper is not just invisible but also not blocking clicks
+          if (wrapperRef.current) {
+            // @ts-expect-error style
+            wrapperRef.current.style.display = 'none';
+          }
+        },
+      });
     },
     { scope: containerRef },
   );
-  // useEffect(() => {
-  //   animatePageIn();
-  // }, []);
+
   return (
     <div ref={containerRef}>
-      <div className="absolute top-0 right-0 bottom-0 left-0 h-screen">
-        <div id="banner-1" ref={banner1Ref} className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-0 w-1/4"></div>
-        <div id="banner-2" ref={banner2Ref} className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-1/4 w-1/4"></div>
-        <div id="banner-3" ref={banner3Ref} className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-2/4 w-1/4"></div>
-        <div id="banner-4" ref={banner4Ref} className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-3/4 w-1/4"></div>
+      <div ref={wrapperRef} className="absolute top-0 right-0 bottom-0 left-0 h-screen">
+        <div id="banner-1" className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-0 w-1/4"></div>
+        <div id="banner-2" className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-1/4 w-1/4"></div>
+        <div id="banner-3" className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-2/4 w-1/4"></div>
+        <div id="banner-4" className="animate-banner min-h-full bg-neutral-950 z-40 fixed top-0 left-3/4 w-1/4"></div>
       </div>
       {children}
     </div>
