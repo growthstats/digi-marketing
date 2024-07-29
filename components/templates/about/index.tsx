b42f70b9a4c0f2d04d3ef5c658f1cce186ee29ca
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-07-29 18:27:43 +0530

    feat: update about page background

--- components/templates/about/index.tsx
+++ components/templates/about/index.tsx
@@ -7,11 +7,14 @@ import HeroSection from '@/components/about/hero-section';
 import TeamSection from '@/components/about/team-section';
 import VisionSection from '@/components/about/vision-section';
 import ContactSection from '@/components/common/contact-section';
+import { cn } from '@/lib/utils';
+
+import styles from './about-page-template.module.scss';
 
 interface IAboutPageTemplateProps {}
 
 const AboutPageTemplate: FC<IAboutPageTemplateProps> = (_props) => (
-  <div>
+  <div className={cn(styles['d-container'])}>
     <HeroSection />
     <AboutSection />
     <VisionSection />
