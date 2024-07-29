1be6073cc74e980acefe6ce077c2f946f88d3d05
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-07-29 17:35:09 +0530

    feat: update about page image

--- components/about/about-section/index.tsx
+++ components/about/about-section/index.tsx
@@ -3,6 +3,8 @@ import cx from 'classnames';
 import Image from 'next/image';
 import { FC } from 'react';
 
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+
 import styles from './about-section.module.scss';
 
 interface IAboutSectionProps {}
@@ -10,14 +12,14 @@ interface IAboutSectionProps {}
 export const AboutSection: FC<IAboutSectionProps> = (_props) => (
   <Section>
     <Box className={cx(styles['d-section__container'])}>
-      <Box className={cx(styles['d-section__col'])}>
-        <Text size={'4'} as="div" mb={'4'}>
+      <Box className={cx(styles['d-section__content'])}>
+        <Text size={'5'} as="div" mb={'4'}>
           <Text weight={'bold'}>Digital Marketing</Text> and <Text weight={'bold'}>3D Animation</Text> companies are popping up like wild
           mushrooms growing in the monsoon. However, when it comes to delivering results, only a handful of companies are capable enough.
           Although it looks simple, digital marketing is not everyone’s cup of tea. It is a web of intricately woven tools that keep
           prospective clients and existing ones engaged with your business to ensure a winning edge over your competitors.
         </Text>
-        <Text size={'4'} as="div" mb={'4'}>
+        <Text size={'5'} as="div" mb={'4'}>
           We design your company website, do{' '}
           <Text weight={'bold'} color="blue">
             Search Engine Optimization
@@ -40,14 +42,14 @@ export const AboutSection: FC<IAboutSectionProps> = (_props) => (
           </Text>{' '}
           that grabs eyeballs, etc. Through a strategic mix of online mediums, we deliver you the expected results.
         </Text>
-        <Text size={'4'} as="div" mb={'4'}>
+        <Text size={'5'} as="div" mb={'4'}>
           Our most <Text weight={'bold'}>Experienced</Text> and <Text weight={'bold'}>Skilled Professionals</Text> ensure that the online
           presence of your business through various tools and promotion of business on the internet will deliver consistent{' '}
           <Text weight={'medium'} color="blue">
             growth.
           </Text>
         </Text>
-        <Text size={'4'} as="div">
+        <Text size={'5'} as="div">
           Since the year 2023, we have been serving the corporate sector by providing all{' '}
           <Text color="blue" weight={'medium'}>
             Online Marketing Solutions
@@ -56,14 +58,16 @@ export const AboutSection: FC<IAboutSectionProps> = (_props) => (
           sole purpose of our existence.
         </Text>
       </Box>
-      <Box className={cx(styles['d-section__col'])}>
-        <Image
-          alt="About Us Section Image"
-          src={'/about/about-company.png'}
-          width={330}
-          height={220}
-          className={cx(styles['d-section__img'])}
-        />
+      <Box className={cx(styles['d-section__image-wrapper'])}>
+        <AspectRatio ratio={0.71 / 1}>
+          <Image
+            alt="About Us Section Image"
+            src={'/about/about-company.png'}
+            width={330}
+            height={220}
+            className={cx(styles['d-section__img'])}
+          />
+        </AspectRatio>
       </Box>
     </Box>
   </Section>
