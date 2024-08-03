a4cb3f917b7d35f5219f39dbf92351400f0b1fd7
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-08-03 17:32:13 +0530

    feat: add fullhuman purgecs

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,17 @@ module.exports = {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+              './app/**/*.{js,jsx,ts,tsx}', // Include other directories if needed
+            ],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
   },
 };
