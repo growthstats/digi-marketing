247db9adf2f11b3a74940fd20fc62c8b3169e515
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-08-09 18:24:20 +0530

    fix: base url config

--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.BASE_URL ?? 'https://growth-stats.vercel.app';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
