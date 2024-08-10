c6910236ab658323081feb7a3de99bb60acde1cc
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-08-10 19:54:04 +0530

    fix: disable gsap markers

--- components/common/service-card/index.tsx
+++ components/common/service-card/index.tsx
@@ -27,7 +27,6 @@ const ServiceCard: FC<IServiceCardProps> = (props) => {
     toVars: { opacity: 1, y: 0, duration: 0.5, delay: 0.3 },
     scrollTriggerOptions: {
       start: 'top 90%',
-      markers: true,
     },
   });
 
