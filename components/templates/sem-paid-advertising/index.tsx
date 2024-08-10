73cc7225ceaffcb62749f56885eb812e502ba9c7
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-08-10 23:08:43 +0530

    feat: add contact section on sem paid advertising template

--- components/templates/sem-paid-advertising/index.tsx
+++ components/templates/sem-paid-advertising/index.tsx
@@ -1,8 +1,10 @@
+import ContactSection from '@/components/common/contact-section';
 import SEMHeroSection from '@/components/sem-paid-advertising/sem-hero-section';
 
 const SEMPaidAdvertisingPageTemplate = () => (
   <div>
     <SEMHeroSection />
+    <ContactSection />
   </div>
 );
 
