c4db0467a1a0a1c34a19f5d2c5dd124d0fbc7648
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-08-20 14:04:22 +0530

    feat: add prod base url constant

--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
+export const PROD_BASE_URL = 'https://growthstats.io';
