2ea8bb51d24d175315811b1b01246a1ffd42e2fc
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-08-23 22:07:52 +0530

    fix: robots sitemap path

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/studio/',
     },
-    sitemap: PROD_BASE_URL,
+    sitemap: `${PROD_BASE_URL}/sitemap.xml`,
   };
 }
